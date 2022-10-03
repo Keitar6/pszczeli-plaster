621e14c38e049c93171c529cfb707c81cdc5a6c1
StatkiewiczMa <mateusz.statkiewicz@capgemini.com>
2022-10-03 15:15:44 +0200

    Implemented persist store

--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,18 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import { Provider } from "react-redux";
-import store from "./store/store.redux";
+import { store, persistor } from "./store/store.redux";
+
+import { PersistGate } from "redux-persist/integration/react";
+
 const container = document.getElementById("root") as HTMLElement;
 const root = createRoot(container);
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
--- src/store/store.redux.ts
+++ src/store/store.redux.ts
@@ -1,15 +1,51 @@
 import { compose, createStore, applyMiddleware } from "redux";
+import type { Middleware } from "redux";
 import logger from "redux-logger";
 import thunk from "redux-thunk";
 
-import rootReducer from "./rootReducer.redux";
+import rootReducer, { ReduxState } from "./rootReducer.redux";
 
-const middleWares = [logger,thunk];
+import { persistStore, persistReducer } from "redux-persist";
+import type { PersistConfig } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
-const composedEnhancer = compose(applyMiddleware(...middleWares));
+declare global {
+  interface Window {
+    __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose;
+  }
+}
 
-const store = createStore(rootReducer, undefined, composedEnhancer);
+type ExtendedPersistConfig = PersistConfig<ReduxState> & {
+  whitelist: (keyof ReduxState)[];
+};
 
-export type AppDispatch = typeof store.dispatch
+const persistConfig: ExtendedPersistConfig = {
+  key: "root",
+  storage: storage, // local storage
+  whitelist: ["cartReducer", "userReducer", "categoriesReducer"]
+};
 
-export default store;
+const middleWares = [
+  process.env.NODE_ENV !== "production" && logger,
+  thunk
+].filter((middleware): middleware is Middleware => Boolean(middleware));
+
+const composeEnhancer =
+  (process.env.NODE_ENV !== "production" &&
+    window &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+  compose;
+
+const composedEnhancers = composeEnhancer(applyMiddleware(...middleWares));
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = createStore(
+  persistedReducer,
+  undefined,
+  composedEnhancers
+);
+
+export const persistor = persistStore(store);
+
+export type AppDispatch = typeof store.dispatch;
